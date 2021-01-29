bfa24aeea87f9521c50afce9ca90b65ed41b3016
thiennguyen1007 <thienhoasac@gmail.com>
2021-01-29 17:39:45 +0700

    tran nop bai

---
+++ Bai046/Bai046/Bai046.Android/obj/Debug/81/android/src/crc64f3e1f54ef4b7f0e6/MainActivity.java
@@ -0,0 +1,47 @@
+package crc64f3e1f54ef4b7f0e6;
+
+
+public class MainActivity
+	extends crc643f46942d9dd1fff9.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("Bai046.Droid.MainActivity, Bai046.Android", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("Bai046.Droid.MainActivity, Bai046.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ Bai046/Bai046/Bai046/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Bai046
+{
+    public partial class MainPage : ContentPage
+    {
+        private int id = 1;
+        public MainPage()
+        {
+            id = 1;
+            InitializeComponent();
+            ImageLoad();
+        }
+
+        private void ImageLoad()
+        {
+
+            anh.Source = ImageSource.FromResource(String.Format("Bai046.MyImage.picture_{0}.jpg",id));
+        }
+
+        private void Previous_Clicked(object sender, EventArgs e)
+        {
+            id--;
+            if (id == 0)
+            {
+                id = 8;
+            }
+            ImageLoad();
+        }
+
+        private void Next_Clicked(object sender, EventArgs e)
+        {
+            id++;
+            if (id == 9)
+            {
+                id = 1;
+            }
+            ImageLoad();
+        }
+    }
+}
